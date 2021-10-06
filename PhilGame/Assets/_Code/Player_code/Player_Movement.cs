ab9f8715127e36f8022972da1d900ffca2715758
Fernando747 <fernandojcr5@gmail.com>
2021-10-06 12:21:30 -0500

    Overhaul of character y

--- PhilGame/Assets/_Code/Player_code/Player_Movement.cs
+++ PhilGame/Assets/_Code/Player_code/Player_Movement.cs
@@ -12,6 +12,8 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] private float gravityValue = -9.81f;
     private Player_Joystick playerInput;
 
+    Rigidbody myRb;
+
     public void Awake()
     {
         playerInput = new Player_Joystick();
@@ -30,19 +32,20 @@ public class Player_Movement : MonoBehaviour
 
     public void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        //groundedPlayer = controller.isGrounded;
+        /*if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
-        }
+        }*/
 
         Vector2 movementInput = playerInput.Joystick_Main.Move.ReadValue<Vector2>();
-        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
+        Vector3 move = new Vector3(0, movementInput.x, 0);
+        // controller.Move(move * Time.deltaTime * playerSpeed);
+        transform.Rotate(move);
         if (move != Vector3.zero)
         {
-            gameObject.transform.forward = move;
+            gameObject.transform.forward = new Vector3(movementInput.x,0,movementInput.y);
+            GetComponent<Rigidbody>().velocity = new Vector3(movementInput.x, 0, movementInput.y);
         }
 
         /* // Changes the height position of the player..
@@ -51,7 +54,7 @@ public class Player_Movement : MonoBehaviour
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
         */
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        //playerVelocity.y += gravityValue * Time.deltaTime;
+        //controller.Move(move * Time.deltaTime * playerSpeed + playerVelocity * Time.deltaTime);
     }
 }
