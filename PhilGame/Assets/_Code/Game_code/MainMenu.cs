c74aacae65745b1a22c76ca9e2f53fef5367b378
Fernando747 <fernandojcr5@gmail.com>
2021-11-30 03:06:14 -0600

    Main menu done

--- PhilGame/Assets/_Code/Game_code/MainMenu.cs
+++ PhilGame/Assets/_Code/Game_code/MainMenu.cs
@@ -5,8 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private AudioSource audioSource;
+    public float secondsToFadeOut;
+
+    public void Start()
+    {
+        audioSource.volume = 1f;
+    }
+
+    public void startGame()
+    {
+        StartCoroutine(FadeOutMusic());
+    }
     public void loadGame()
     {
         SceneManager.LoadScene("01_Main_Game");
+    }
+
+    private IEnumerator FadeOutMusic()
+    {
+        while (audioSource.volume > 0.01f)
+        {
+            audioSource.volume -= Time.deltaTime / secondsToFadeOut;
+            yield return null;
+        }
+        loadGame();
     }
 }
